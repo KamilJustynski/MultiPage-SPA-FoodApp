d3968ed820ef2eee7cc56e6d5521690f1d89f377
Kamil Justynski <kamil.justynski1998@gmail.com>
2025-01-19 17:44:00 +0100

    relative and absolute paths

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { HomePage } from "./pages/Home";
 import { ProductsPage } from "./pages/Products";
 import { RootLayout } from "./pages/Root";
 import { ErrorPage } from "./pages/Error";
+import { ProductDetail } from "./pages/ProductDetal";
+
+//! z / - ściezka bezwzgledna, nie opiera sie na poprzedniej, po prostu sie dodaje
+//! bez / - sciezka wzgledna, ktora opiera sie na poprzedniej i dodaje do niej
+
+//! index = true wysweitla domyslnie aktywna sciezke ktora zostala zadeklarowana w rodzicu!, zaladuje nam to komponent ktory jest okreslony, jesli sciezka rodzica jest aktywna
 
 const router = createBrowserRouter([
   {
@@ -10,11 +16,12 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
+      { index: true, element: <HomePage /> }, //!path: '' alternatywa index: true
       {
-        path: "/products",
+        path: "products",
         element: <ProductsPage />,
       },
+      { path: "products/:productId", element: <ProductDetail /> },
     ],
   },
 ]);
